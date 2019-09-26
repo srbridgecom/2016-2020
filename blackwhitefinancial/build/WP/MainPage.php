23177563ec5129c7f27b9eb6ee8a67156473fff6
R.J. Bridge <ryanjbcom@users.noreply.github.com>
2019-09-25 20:36:50 -0700

    Create MainPage.php

---
+++ blackwhitefinancial/build/WP/MainPage.php
@@ -0,0 +1,34 @@
+<p>
+</p>
+<div class="wp-block-image">
+<figure class="aligncenter"><img class="wp-image-9" src="https://srbridge.com/clients/blackwhite/wp-content/uploads/2019/03/1965-washington-quarter.png" alt=""></figure>
+</div>
+
+<!-- wp:spacer {"height":61} -->
+<div style="height:61px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<h3 style="text-align: center;"><span style="color: #000000;"><strong>We are a resource for anyone looking to fine tune their knowledge and skills on finances.</strong></span></h3>
+<h3 style="text-align: center;"><span style="color: #000000;">Learn to pay off you debt, manage your finances and repair your credit.</span></h3>
+
+<!-- wp:spacer {"height":61} -->
+<div style="height:61px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:html -->
+<div class="row">
+    <div class="col-6 col-md-4">
+      <img src="https://srbridge.com/clients/blackwhite/wp-content/uploads/2019/03/know.png">
+    </div>
+    <div class="col-6 col-md-4">
+      <img src="https://srbridge.com/clients/blackwhite/wp-content/uploads/2019/03/pie.png">
+    </div>
+    <div class="col-6 col-md-4">
+      <img src="https://srbridge.com/clients/blackwhite/wp-content/uploads/2019/03/checklist.png">
+    </div>
+  </div>
+<!-- /wp:html -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
